00697c17535f72ce5bdf11ee6c314b40a17cafcf
Marco Bardelli <bardelli.marco@gmail.com>
2022-08-29 17:55:06 +0200

    keep the banner section on all pages

--- webui/src/routes/__layout.svelte
+++ webui/src/routes/__layout.svelte
@@ -8,7 +8,10 @@
   import { page } from '$app/stores';
   import { serviceClients, epochData, mainQueueMembers, addonQueueMembers, supportersList, cardanoWallet } from '$lib/stores';
   import { doCallInPromise } from '$lib/utils';
-  import {ControlMsg} from '$lib/pb/control_pb';
+  import { User, ControlMsg } from '$lib/pb/control_pb';
+
+  $: mainServed = $mainQueueMembers[0];
+  $: user = $cardanoWallet.user
 
   if ($serviceClients.Control === null) {
       serviceClients.getControlServiceClient($page.url.origin)
@@ -58,6 +61,34 @@
   {#if $page.url.pathname !== '/' && $page.url.pathname !== '/about'}
     <UserBox user={$cardanoWallet.user} />
   {/if}
+
+<section class="section">
+  <div class="columns m-0 has-text-centered">
+    <div class="column">
+      <h1>Welcome to F2LB (unofficial) Website</h1>
+      <p>Visit <a href="https://www.f2lb.org/">F2LB</a> to read about the project and the community</p>
+      <p>Visit also <a href="https://f2lb.info/">F2LB.info</a></p>
+      {#if user !== undefined}
+      <br />
+      <div class="box box-out box-user">
+      {#if user.type === User.Type.VISITOR}
+        <p>Hello <span class="text truncate">{user.stakeaddress}</span> !</p>
+        <p>Thanks to visit this website, and if you did not do yet, please join our community.</p>
+        <p>If you want to contribute to ensure Cardano decentralization, consider to be a sponsor of F2LB Community.</p>
+        <p>If you are a small and single pool operator, join us ...</p>
+      {:else if user.type === User.Type.SUPPORTER}
+        <p>Hello <span class="text truncate">{user.supporter.discordid}</span> !</p>
+        <p>Thanks for supporting our community.</p>
+      {:else if user.type === User.Type.SPO}
+        <p>Hello <span class="text truncate">{user.member.ticker}</span> !</p>
+        <p>Thanks to visit this website, your status is explained below ...</p>
+      {/if}
+      <button class="button" disabled>Delegate to the SPO on top of the queue, currently {mainServed.ticker}</button>
+      </div>
+      {/if}
+    </div>
+  </div>
+</section>
 
   <slot />
 </main>
